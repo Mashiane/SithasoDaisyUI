601be62abd6718fc82dc8ea8afc087c1d79cf7fe
Mashy <mbanga.anele@gmail.com>
2023-09-13 21:28:16 +0200

    SithasoDaisy 2.00

---
+++ assets/storeimage.php
@@ -0,0 +1,16 @@
+<?php    
+    $img = $_POST['image'];
+    $folderPath = "uploads/";
+  
+    $image_parts = explode(";base64,", $img);
+    $image_type_aux = explode("image/", $image_parts[0]);
+    $image_type = $image_type_aux[1];
+  
+    $image_base64 = base64_decode($image_parts[1]);
+    $fileName = uniqid() . '.png';
+  
+    $file = $folderPath . $fileName;
+    file_put_contents($file, $image_base64);
+  
+    print_r($fileName);  
+?>
