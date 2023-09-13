a13f694559dd21113effe0a15d344325abc581dc
Mashy <mbanga.anele@gmail.com>
2023-09-13 21:09:55 +0200

    Delete assets directory

--- assets/msaccessconfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-const DB_NAME = 'C:\\laragon\\www\\bvad3msaccessphp\\assets\\estore.accdb';
-const DB_USER = '';
-const DB_PASS = '';
-?>
--- assets/mysqlconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-const DB_HOST = 'localhost';
-const DB_NAME = 'wardlog';
-const DB_USER = 'root';
-const DB_PASS = '';
-?>
